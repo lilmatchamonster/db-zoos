a6baeeb0f813b5811471fb4ea0f44279ee10404f
Ashley Owens <ash4741@gmail.com>
2019-03-16 16:02:30 -0400

    Complete MVP

--- index.js
+++ index.js
@@ -25,22 +25,63 @@ server.get('/api/zoos', async (req, res) => {
     res.status(200).json(zoos);
   }
   catch (error) {
-    res.status(500).json(error);
+    res.status(500).json({message: "Error retriving data"});
+  }
+});
+
+//Get by id Function
+server.get('/api/zoos/:id', async (req, res) => {
+  try {
+    const zoo = await db('zoos')
+    .where({ id: req.params.id }); //Return record that matches specific ID from table 
+    
+      res.status(200).json(zoo);
+  }
+  catch (error) {
+    res.status(500).json({message: "Error retriving data"});
   }
 });
 
 //Post Function
 server.post('/api/zoos', async (req, res) => {
   try{
-    const zoo = await db('zoos').insert({ name: req.body.name });
+    const zoo = await db('zoos')
+    .insert({ name: req.body.name });
+
     res.status(201).json(zoo);
+  }
+  catch (error) {
+    res.status(500).json({message: "Error adding zoo"});
+  }
+});
+
+//Put Function
+server.put('/api/zoos/:id', async (req, res) => {
+  try{
+    const zoo = await db('zoos')
+    .where({ id: req.params.id })
+    .update(req.body);
+
+    res.status(200).json(zoo);
   }
   catch (error) {
     res.status(500).json(error);
   }
 });
 
+//Delete Function
+server.delete('/api/zoos/:id', async (req, res) => {
+  try{
+    const zoo = await db('zoos')
+    .where({ id: req.params.id })
+    .del();
 
+    res.status(204).end();
+  }
+  catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 const port = 3300;
 server.listen(port, function() {
